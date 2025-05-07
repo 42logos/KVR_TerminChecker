43dfb7bf87e702b81885edfb9dead33670ea1307
YUANLUO WU <yuanluowu@gmail.com>
2025-05-07 18:57:58 +0200

    Fix output encoding configuration in logger setup

--- src/Main.py
+++ src/Main.py
@@ -27,6 +27,7 @@ import random
 
 import threading
 logger.remove()  # 移除默认的日志处理器
+sys.out.reconfigure(encoding='utf-8')  # 设置输出编码为 utf-8
 
 logger.add(
     sink="logs/{time:YYYY-MM-DD}.log",
@@ -39,8 +40,7 @@ logger.add(
 logger.add(
         sink=sys.stdout,
         level="INFO",
-        encoding="utf-8",
-
+    
     )
 
 logger.debug("Logger initialized with name: Main")
