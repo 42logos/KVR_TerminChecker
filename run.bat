22a036eb7467d6ba22c85f702176f3a4085feb0c
YUANLUO WU <yuanluowu@gmail.com>
2025-05-09 02:43:04 +0200

    Refactor run.bat to include Chrome installation check and improve Python installation prompts

--- run.bat
+++ run.bat
@@ -1,12 +1,29 @@
 
 @echo off
+REM Check if Chrome is installed
+IF NOT EXIST "C:\Program Files\Google\Chrome\Application\chrome.exe" (
+    echo Google Chrome is not installed. Please install it from https://www.google.com/chrome/
+    
+    echo try using chocolatey to install chrome
+    echo Check if chocolatey is installed
+    choco --version >nul 2>&1
+    IF %ERRORLEVEL% NEQ 0 (
+        echo Chocolatey is not installed. Try install chocolatey from https://chocolatey.org/install
+        @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+    )
+    choco install googlechrome -y
+)
+
 REM Check if Python is installed
 python --version >nul 2>&1
 IF %ERRORLEVEL% NEQ 0 (
-    echo Python is not installed. Try install python through chocolatey.
-    echo install chocolatey from https://chocolatey.org/install
-    @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-    
+    echo Python is not installed. Try install uv through chocolatey.
+    echo Check if chocolatey is installed
+    choco --version >nul 2>&1
+    IF %ERRORLEVEL% NEQ 0 (
+        echo Chocolatey is not installed. Try install chocolatey from https://chocolatey.org/install
+        @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+    )
 
     REM Check if uv is installed
     uv --version >nul 2>&1
@@ -16,29 +33,9 @@ IF %ERRORLEVEL% NEQ 0 (
     )
     
     uv run src/Main.py
-    IF %ERRORLEVEL% NEQ 0 (
-        echo Failed to install Python.
-        exit /b %ERRORLEVEL% 
-    )
     )
 else (
-    echo Python is already installed.
-)
-
-REM Check if chromedriver is installed
-IF NOT EXIST "chromedriver.exe" (
-    echo Chromedriver is not installed. Installing...
-    REM Install chromedriver using Chocolatey
-    choco install chromedriver -y
-)
-
-REM Exit
-IF %ERRORLEVEL% NEQ 0 (
-    echo Failed to install chromedriver.
-    exit /b %ERRORLEVEL%
-)
-
-REM Check if virtual environment exists
+    REM Check if virtual environment exists
 IF NOT EXIST "venv\Scripts\activate.bat" (
     echo Creating virtual environment...
     python -m venv venv
@@ -59,3 +56,7 @@ pip install -r requirements.txt
 
 REM Run Main.py
 python src/Main.py
+)
+
+
+
